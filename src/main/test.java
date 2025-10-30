a775f3552cc64b86bad2b0f4cf86469ad72880ae
yzj56 <2402496386@qq.com>
2025-10-30 08:08:18 +0800

    2025.10.30.8:01  212306341 杨则健

---
+++ src/main/test.java
@@ -0,0 +1,6 @@
+public  public static void start(android.content.Context context) {
+    android.content.Intent starter = new Intent(context, .class);
+    starter.putExtra();
+    System.out.println("2025.10.30.8:01  212306341 杨则健")
+    context.startActivity(starter);
+}
